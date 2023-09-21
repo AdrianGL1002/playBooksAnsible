6e1618dbcc6e6f9b1567a1ee294ec94995a8637c
AdrianGL1002 <124020162+AdrianGL1002@users.noreply.github.com>
2023-09-20 20:59:44 -0600

    Add files via upload
    Todos los playbook usados y el archivo dump.sql

---
+++ dump.sql
@@ -0,0 +1,2 @@
+CREATE TABLE IF NOT EXISTS datos (nombre VARCHAR (155), correo VARCHAR (155), mensaje VARCHAR (500));
+  INSERT INTO datos (nombre, correo, mensaje) VALUES('Rosa', 'rosalopez@gmail.com', 'Hola como estas?, soy Rosa');
